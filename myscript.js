8fdec0870cb57030f58a49532486ac1f87eebacf
Nicole Kohoutek <nicole.kohoutek@gmail.com>
2017-12-23 12:24:29 +0100

    add new files for codereview5

---
+++ myscript.js
@@ -0,0 +1,38 @@
+var codersMates = [
+                        {
+			name: 'John',
+			surname: 'Doe',
+			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+			age: 25,
+   			myPhoto: "img/boy1.jpg",	
+			likes: 1,    // numeric property used to store likes
+			},
+
+						{
+			name: 'Jane',
+			surname: 'Doe',
+			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+			age: 25,
+   			myPhoto: "img/girl1.jpg",	
+			likes: 2,   
+			},
+
+			{
+			name: 'Max',
+			surname: 'Musterman',
+			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+			age: 25,
+   			myPhoto: "img/boy2.jpg",	
+			likes: 4,   
+			},
+
+			{
+			name: 'Maxina',
+			surname: 'Musterman',
+			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+			age: 25,
+   			myPhoto: "img/girl2.jpg",	
+			likes: 3,  
+			}
+
+			];
